7c9c8ed1b8f94e9983b3da2f4729e320d560f520
ErdemInal <77430973+ErdemInal@users.noreply.github.com>
2022-06-15 12:32:20 +0300

    aciliyet update

--- YSKProje.ToDo.Web/Areas/Admin/Controllers/AciliyetController.cs
+++ YSKProje.ToDo.Web/Areas/Admin/Controllers/AciliyetController.cs
@@ -20,6 +20,7 @@ namespace YSKProje.ToDo.Web.Areas.Admin.Controllers
         }
         public IActionResult Index()
         {
+            TempData["Active"] = "aciliyet";
             List<Aciliyet> aciliyetler = _aciliyetService.GetirHepsi();
 
             List<AciliyetListViewModel> model = new List<AciliyetListViewModel>();
@@ -33,6 +34,56 @@ namespace YSKProje.ToDo.Web.Areas.Admin.Controllers
                 model.Add(aciliyetModel);
             }
             return View(model);
+        }
+
+        public IActionResult EkleAciliyet()
+        {
+            TempData["Active"] = "aciliyet";
+            return View(new AciliyetAddViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult EkleAciliyet(AciliyetAddViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _aciliyetService.Kaydet(new Aciliyet()
+                {
+                    Tanim = model.Tanim
+                });
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        public IActionResult GuncelleAciliyet(int id)
+        {
+            TempData["Active"] = "aciliyet";
+            var aciliyet = _aciliyetService.GetirIdile(id);
+            AciliyetUpdateViewModel model = new AciliyetUpdateViewModel()
+            {
+                Id = aciliyet.Id,
+                Tanim = aciliyet.Tanim
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult GuncelleAciliyet(AciliyetUpdateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _aciliyetService.Guncelle(new Aciliyet()
+                {
+                    Id = model.Id,
+                    Tanim = model.Tanim
+
+                });
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+
         }
     }
 }
---
+++ YSKProje.ToDo.Web/Areas/Admin/Controllers/GorevController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YSKProje.ToDo.Business.Interfaces;
+using YSKProje.ToDo.Entities.Concrete;
+using YSKProje.ToDo.Web.Areas.Admin.Models;
+
+namespace YSKProje.ToDo.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class GorevController : Controller
+    {
+        private readonly IGorevService _gorevService;
+
+        public GorevController(IGorevService gorevService)
+        {
+            _gorevService = gorevService;
+        }
+        public IActionResult Index()
+        {
+            TempData["Active"] = "gorev";
+            List<Gorev> gorevler = _gorevService.GetirHepsi();
+            List<GorevListViewModel> models = new List<GorevListViewModel>();
+
+            foreach (var item in gorevler)
+            {
+                GorevListViewModel model = new GorevListViewModel
+                {
+                    Aciklama = item.Aciklama,
+                    Aciliyet = item.Aciliyet,
+                    AciliyetId = item.AciliyetId,
+                    Ad = item.Ad,
+                    Durum = item.Durum,
+                    Id = item.Id,
+                    OlusturulmaTarih = item.OlusturulmaTarih
+                };
+                models.Add(model);
+
+            }
+            return View(models);
+        }
+    }
+}
