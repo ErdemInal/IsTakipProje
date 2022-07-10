c9d82d21b0ff621ea36818ca1206b7410cc1d7f8
ErdemInal <77430973+ErdemInal@users.noreply.github.com>
2022-07-10 21:56:24 +0300

    resim ekleme islemleri

---
+++ YSKProje.ToDo.Web/Areas/Admin/Controllers/ProfilController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using YSKProje.ToDo.Entities.Concrete;
+using YSKProje.ToDo.Web.Areas.Admin.Models;
+
+namespace YSKProje.ToDo.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class ProfilController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        public ProfilController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            TempData["Active"] = "profil";
+            var appUser = await _userManager.FindByNameAsync(User.Identity.Name);
+            AppUserListViewModel model = new AppUserListViewModel();
+            model.Id = appUser.Id;
+            model.Name = appUser.Name;
+            model.Surname = appUser.Surname;
+            model.Picture = appUser.Picture;
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(AppUserListViewModel model,IFormFile Resim)
+        {
+            if (ModelState.IsValid)
+            {
+                var guncellenecekKullanici = _userManager.Users.FirstOrDefault(I => I.Id == model.Id);
+                if (Resim != null)
+                {
+                    string uzanti = Path.GetExtension(Resim.FileName);
+                    string resimAd = Guid.NewGuid() + uzanti;
+                    string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/" + resimAd);
+                    using (var straam = new FileStream(path,FileMode.Create))
+                    {
+                        await Resim.CopyToAsync(straam);
+                    }
+
+                    guncellenecekKullanici.Picture = resimAd;
+                }
+
+                guncellenecekKullanici.Name = model.Name;
+                guncellenecekKullanici.Surname = model.Surname;
+                guncellenecekKullanici.Email = model.Email;
+
+                var result = await _userManager.UpdateAsync(guncellenecekKullanici);
+                if (result.Succeeded)
+                {
+                    TempData["message"] = "Güncelleme işleminiz başarı ile gerçekleşti.";
+                    return RedirectToAction("Index");
+                }
+
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+            }
+            return View(model);
+        }
+    }
+}
